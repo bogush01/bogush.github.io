0d15ec6ffb8662a017e34e41216ee61c68640509
Advolatum-front <Advolatum-front@yandex.ru>
2024-03-24 13:51:08 +0500

    create header structure

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,10 +1,34 @@
+import { Link } from "react-router-dom";
+
 import "./index.css";
 
 const Header = () => {
   return (
-    <>
-      <header className="header"></header>
-    </>
+    <header className="header">
+      <div>
+        <Link to="/">q</Link>
+        <nav>
+          <ul>
+            <li>Главная</li>
+            <li>Коллекция</li>
+            <li>O нас</li>
+            <li>Контакты</li>
+            <li>Корзина</li>
+          </ul>
+        </nav>
+        <div>
+          <a href="#">1</a>
+          <a href="#">1</a>
+          <a href="#">1</a>
+          <a href="#">1</a>
+        </div>
+      </div>
+      <ul>
+        <li>Девушкам</li>
+        <li>Мужчинам</li>
+        <li>Унисекс</li>
+      </ul>
+    </header>
   );
 };
 
