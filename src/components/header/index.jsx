9cfd13a0e939e9d52ec82ddd8ca5ee2f0ed05bbd
Advolatum-front <Advolatum-front@yandex.ru>
2024-03-24 16:55:40 +0500

    complete header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,32 +1,89 @@
 import { Link } from "react-router-dom";
 
+import logoPic from "../../img/logo.png";
+
+import { ReactComponent as SocVk } from "../../svg/socials/ico_vk.svg";
+import { ReactComponent as SocTg } from "../../svg/socials/ico_tg.svg";
+import { ReactComponent as SocInst } from "../../svg/socials/ico_inst.svg";
+import { ReactComponent as SocTiktok } from "../../svg/socials/ico_tiktok.svg";
+
 import "./index.css";
 
 const Header = () => {
   return (
     <header className="header">
-      <div>
-        <Link to="/">q</Link>
-        <nav>
-          <ul>
-            <li>Главная</li>
-            <li>Коллекция</li>
-            <li>O нас</li>
-            <li>Контакты</li>
-            <li>Корзина</li>
+      <div className="header__top-part">
+        <Link to="/">
+          <img className="header__logo" src={logoPic} alt="" />
+        </Link>
+        <nav className="header__main-menu-conainer">
+          <ul className="header__main-menu">
+            <li>
+              <Link className="header__main-menu-link" to="/">
+                Главная
+              </Link>
+            </li>
+            <li>
+              <Link className="header__main-menu-link" to="/">
+                Коллекция
+              </Link>
+            </li>
+            <li>
+              <Link className="header__main-menu-link" to="/">
+                O нас
+              </Link>
+            </li>
+            <li>
+              <Link className="header__main-menu-link" to="/">
+                Контакты
+              </Link>
+            </li>
+            <li>
+              <Link className="header__main-menu-link" to="/">
+                Корзина
+              </Link>
+            </li>
           </ul>
         </nav>
-        <div>
-          <a href="#">1</a>
-          <a href="#">1</a>
-          <a href="#">1</a>
-          <a href="#">1</a>
-        </div>
+        <ul className="social-buttons">
+          <li>
+            <a href="#">
+              <SocVk />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <SocTg />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <SocInst />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <SocTiktok />
+            </a>
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li>Девушкам</li>
-        <li>Мужчинам</li>
-        <li>Унисекс</li>
+      <ul className="header__gender-menu">
+        <li>
+          <Link className="header__gender-menu-link" to="/">
+            Девушкам
+          </Link>
+        </li>
+        <li>
+          <Link className="header__gender-menu-link" to="/">
+            Мужчинам
+          </Link>
+        </li>
+        <li>
+          <Link className="header__gender-menu-link" to="/">
+            Унисекс
+          </Link>
+        </li>
       </ul>
     </header>
   );
