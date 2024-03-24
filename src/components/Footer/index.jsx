82a28772d44e56041aa9063d2a194cc7a67c88f2
Advolatum-front <Advolatum-front@yandex.ru>
2024-03-25 00:17:11 +0500

    complete footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,10 +2,32 @@ import { Link } from "react-router-dom";
 
 import Socials from "../Socials";
 
+import logoFooter from "../../img/logo_footer.png";
+
 import "./index.css";
 
 const Footer = () => {
-  return <footer className="footer"></footer>;
+  return (
+    <footer className="footer">
+      <ul className="footer__policy">
+        <li>
+          <Link className="footer__policy-link" to="/">
+            Политика конфиденциальности
+          </Link>
+        </li>
+        <li>
+          <Link className="footer__policy-link" to="/">
+            Согласие на обработку конфидециальных данных
+          </Link>
+        </li>
+      </ul>
+      <img className="footer__logo" src={logoFooter} alt="" />
+      <div>
+        <Socials />
+      </div>
+      <p className="footer__copyright">2024 GruboGovorya</p>
+    </footer>
+  );
 };
 
 export default Footer;
