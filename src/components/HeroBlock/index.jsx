6a67ae0e10ca97eac17e0fe05cdbafc65c46eb0f
Advolatum-front <Advolatum-front@yandex.ru>
2024-03-25 13:03:50 +0500

    create back-text header in heroblock

--- src/components/HeroBlock/index.jsx
+++ src/components/HeroBlock/index.jsx
@@ -1,7 +1,26 @@
+import logo from "../../img/logo.png";
+import clothesForEveryone from "../../img/clothes_for_everyone.png";
+
 import "./index.css";
 
 const HeroBlock = () => {
-  return <div className="hero-block">хироБлок</div>;
+  return (
+    <div className="hero-block">
+      <img className="hero-block__logo" src={logo} alt="" />
+      <div className="hero-block__back-text-header">
+        <div className="back-text-header">
+          <p className="back-text">Одежда для каждого</p>
+          <div className="back-text-main-content-container">
+            <img
+              className="back-text-main-content"
+              src={clothesForEveryone}
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroBlock;
