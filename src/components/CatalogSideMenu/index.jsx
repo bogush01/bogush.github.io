a701fe68d63f6b3f44042d7781fbf854086c9b7b
Advolatum-front <Advolatum-front@yandex.ru>
2024-03-27 00:50:36 +0500

    apply styles to side menu

--- src/components/CatalogSideMenu/index.jsx
+++ src/components/CatalogSideMenu/index.jsx
@@ -3,7 +3,38 @@ import { Link } from "react-router-dom";
 import "./index.css";
 
 const CatalogSideMenu = () => {
-  return <span>Это менюшка</span>;
+  return (
+    <ul className="catalog-side-menu">
+      <li>
+        <Link className="catalog-side-menu-link" to="/">
+          Плащи
+        </Link>
+      </li>
+      <li>
+        <Link className="catalog-side-menu-link" to="/">
+          Куртки
+        </Link>
+      </li>
+      <li>
+        <span className="catalog-side-menu-active">Кофты</span>
+      </li>
+      <li>
+        <Link className="catalog-side-menu-link" to="/">
+          Комбинезоны
+        </Link>
+      </li>
+      <li>
+        <Link className="catalog-side-menu-link" to="/">
+          Кепки
+        </Link>
+      </li>
+      <li>
+        <Link className="catalog-side-menu-link" to="/">
+          Сумки
+        </Link>
+      </li>
+    </ul>
+  );
 };
 
 export default CatalogSideMenu;
