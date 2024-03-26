6d83b1af0f5d839cb1ee85faf660ee7c233f1396
Advolatum-front <Advolatum-front@yandex.ru>
2024-03-26 21:01:49 +0500

    prepare contacts section

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 import HeroBlock from "../../components/HeroBlock";
 import BackTextHeader from "../../components/BackTextHeader";
 import CurrentProductsList from "../../components/CurrentProductsList";
+import Socials from "../../components/Socials";
 
 import mainCarouselPreview from "../../img/main-carousel/main_carousel_preview.png";
+import map from "../../img/map.jpg";
 
 import "./index.css";
 
@@ -43,6 +45,30 @@ const MainPage = () => {
           <Link className="link-show-more" to="/">
             Смотреть больше
           </Link>
+        </div>
+      </section>
+      <section className="section">
+        <BackTextHeader
+          className="back-text-header-class"
+          backText="Контакты"
+          backTextClassName="back-text-class-contacts"
+          childrenContainerClassName="children-container-left"
+        >
+          <h2 className="children-class">Мы всегда на связи</h2>
+        </BackTextHeader>
+        <div className="connection-block">
+          <img src={map} alt="" />
+          <ul className="contacts">
+            <li>
+              <a href="tel:+78000000000">8 (800) 000-00-00</a>
+            </li>
+            <li>
+              <a href="mailto:info@advolatum.ru">info@advolatum.ru</a>
+            </li>
+            <li>
+              <Socials />
+            </li>
+          </ul>
         </div>
       </section>
     </>
