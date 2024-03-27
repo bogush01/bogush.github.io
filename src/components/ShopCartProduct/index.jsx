c02e1d0a4d0ae68f5fd1df09a0ae9d4a1cc37441
Advolatum-front <Advolatum-front@yandex.ru>
2024-03-27 23:53:13 +0500

    complete shopCartProduct and shopCartList

--- src/components/ShopCartProduct/index.jsx
+++ src/components/ShopCartProduct/index.jsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
+
+import photo from "../../img/shop_cart/photo1.jpg";
+
 import "./index.css";
 
 const ShopCartProduct = () => {
-  return <div className="shop-cart-product"></div>;
+  return (
+    <div className="shop-cart-product">
+      <Link to="/">
+        <img className="shop-cart-product__photo" src={photo} alt="" />
+      </Link>
+      <div className="shop-cart-product__about">
+        <Link to="/" className="shop-cart-product__title">
+          Костюм Advolatum — соцветие Костюм Advolatum — соцветие Костюм
+          Advolatum — соцветие
+        </Link>
+        <ul className="shop-cart-product__features">
+          <li>Размер: M</li>
+          <li>Кол-во: - 1 +</li>
+          <li>Стоимость: 3751 </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default ShopCartProduct;
