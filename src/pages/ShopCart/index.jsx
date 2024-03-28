7a7d10750dd7c41531743ce4552d37a227e645e9
Advolatum-front <Advolatum-front@yandex.ru>
2024-03-28 12:25:40 +0500

    fix shopCart page structure

--- src/pages/ShopCart/index.jsx
+++ src/pages/ShopCart/index.jsx
@@ -5,22 +5,28 @@ import "./index.css";
 
 const ShopCart = () => {
   return (
-    <div className="shop-cart">
+    <>
       <BackTextHeader
+        className="shop-cart__header"
         backText="Корзина"
         backTextClassName="shop-cart-back-text"
       />
-      <ShopCartList className="shop-cart__list" />
-      <div className="shop-cart__bill">
-        <p className="shop-cart__bill-text">
-          Сумма доставки будет расчитана после ввода данных о месте назначения —
-          город, улица
-        </p>
-        <dl className="shop-cart__bill-items">
-          <div className="shop-cart__bill-item"></div>
-        </dl>
+      <div className="shop-cart">
+        <div className="shop-cart__cart-content">
+          <ShopCartList className="shop-cart__list" />
+          <div className="shop-cart__bill">
+            <p className="shop-cart__bill-text">
+              Сумма доставки будет расчитана после ввода данных о месте
+              назначения — город, улица
+            </p>
+            <dl className="shop-cart__bill-items">
+              <div className="shop-cart__bill-item"></div>
+            </dl>
+          </div>
+        </div>
+        <form>форма</form>
       </div>
-    </div>
+    </>
   );
 };
 
