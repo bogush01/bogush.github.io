7e0daa6969926d48dfced3e78d170b5b9c8df4ee
Advolatum-front <Advolatum-front@yandex.ru>
2024-03-30 02:02:15 +0500

    complete collections page

--- src/pages/Collections/index.jsx
+++ src/pages/Collections/index.jsx
@@ -4,6 +4,11 @@ import CurrentProductsList from "../../components/CurrentProductsList";
 import BackTextHeader from "../../components/BackTextHeader";
 import SectionContacts from "../../components/SectionContacts";
 
+import pic1 from "../../img/current_collection/1.jpg";
+import pic2 from "../../img/current_collection/2.jpg";
+import pic3 from "../../img/current_collection/3.jpg";
+import pic4 from "../../img/current_collection/4.jpg";
+
 import "./index.css";
 
 const Collections = () => {
@@ -19,6 +24,21 @@ const Collections = () => {
           <Link className="current-collection__other-collections">
             Другие коллекции
           </Link>
+        </div>
+        <p className="current-collection__description">
+          коллекция была создана для людей учитывающие детали в своей
+          повседневной жизни В перфомансе стиля мы хотели приподнести легкость и
+          комфорт, к которому стремится каждый
+        </p>
+        <div className="current-collection__colums">
+          <div className="current-collection__left-col">
+            <img src={pic1} alt="" />
+            <img src={pic2} alt="" className="current-collection__photo2" />
+            <img src={pic3} alt="" />
+          </div>
+          <div className="current-collection__right-col">
+            <img src={pic4} alt="" className="current-collection__photo4" />
+          </div>
         </div>
       </section>
 
