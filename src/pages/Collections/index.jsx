13e0bfafa997d87631870f375d6d602e1c07fa33
Advolatum-front <Advolatum-front@yandex.ru>
2024-03-29 15:03:54 +0500

    add contacts section and collestiong products

--- src/pages/Collections/index.jsx
+++ src/pages/Collections/index.jsx
@@ -1,7 +1,27 @@
+import CurrentProductsList from "../../components/CurrentProductsList";
+import BackTextHeader from "../../components/BackTextHeader";
+import SectionContacts from "../../components/SectionContacts";
+
 import "./index.css";
 
 const Collections = () => {
-  return <span>Коллекции!</span>;
+  return (
+    <>
+      <section className="collection-products">
+        <BackTextHeader
+          className="collection-products__header"
+          backText="Актуальное"
+          childrenContainerClassName="collection-products__children-container"
+        >
+          <h2 className="collection-products__children-class">
+            Товары коллекции
+          </h2>
+        </BackTextHeader>
+        <CurrentProductsList />
+      </section>
+      <SectionContacts />
+    </>
+  );
 };
 
 export default Collections;
