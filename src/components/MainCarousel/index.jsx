7f473ca3a18f2b16b47707188ca752ed19639b52
Advolatum-front <Advolatum-front@yandex.ru>
2024-03-30 21:35:17 +0500

    add slider info block

--- src/components/MainCarousel/index.jsx
+++ src/components/MainCarousel/index.jsx
@@ -1,18 +1,21 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import StyledButtonOrLink from "../../components/controls/StyledButtonOrLink";
 
 import mainCarouselPreview from "../../img/main-carousel/main_carousel_preview.png";
 
 import "./index.css";
 
-const SLIDES_COUNT = 1;
+const SLIDES_COUNT = 5;
 
 const MainCarousel = () => {
-  const handleSwitchChange = (e) => {
-    console.clear();
-    console.log(e.target.dataset.index);
-  };
-
   const [selectedSlide, setSelectedSlide] = useState(0);
+
+  const handleSwitchChange = (event) => {
+    const newIndex = Number(event.target.dataset.index);
+    setSelectedSlide(newIndex);
+  };
 
   const switchButtons = Array(SLIDES_COUNT)
     .fill(null)
@@ -38,7 +41,19 @@ const MainCarousel = () => {
   return (
     <div className="main-carousel">
       <ul className="main-carousel__slides-container">
-        <li className="main-carousel__slide slide1"></li>
+        <li className="main-carousel__slide slide1">
+          <div className="main-carousel__slide-info">
+            <p className="main-carousel__collection-description">
+              Текст описания конкретной коллекции с возможным дополнением
+              уникальности
+            </p>
+            <StyledButtonOrLink
+              className="main-carousel__slide-catalog-link"
+              caption="Каталог"
+              to="/catalog"
+            />
+          </div>
+        </li>
       </ul>
 
       <ul className="main-carousel__swicth">{switchButtons}</ul>
