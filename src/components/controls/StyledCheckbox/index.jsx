1aa8af78d885bae3fbaa3ef5a38105b9d3e08709
Advolatum-front <Advolatum-front@yandex.ru>
2024-04-03 12:50:07 +0500

    fix styled checkbox one more time

--- src/components/controls/StyledCheckbox/index.jsx
+++ src/components/controls/StyledCheckbox/index.jsx
@@ -1,17 +1,15 @@
 import "./index.css";
 
 const StyledCheckbox = (props) => {
-  const { className = "", captionClassName = "", caption = "" } = props;
+  const { className = "", caption = "" } = props;
 
   return (
     <label className={`styled-checkbox ${className}`}>
       <input type="checkbox" className="styled-checkbox__checkbox" />
-      <span className="styled-checkbox__checkbox-wrapper">
+      <span className="styled-checkbox__fake-checkbox-wrapper">
         <span className="styled-checkbox__fake-checkbox"></span>
       </span>
-      <span className={`styled-checkbox__caption ${captionClassName}`}>
-        {caption}
-      </span>
+      <span className="styled-checkbox__caption">{caption}</span>
     </label>
   );
 };
